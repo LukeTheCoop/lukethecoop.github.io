40c69100c7807557a9ae698c167653b2ff126958
LukeTheCoop <lukeisthecoop@gmail.com>
2024-02-07 10:33:53 -0800

    Update 2/7/24 Added new css format. Title Page.

--- js/script.js
+++ js/script.js
@@ -1,51 +1,44 @@
-// Load the initial poem, god, and philosophy preview
-document.addEventListener("DOMContentLoaded", function() {
-  fetch('/static/writings/poems.json')
-    .then(response => response.json())
-    .then(poems => {
-      // Get the first poem
-      const firstPoem = poems[0];
-      // Extract the first 200 words
-      const poemPreview = firstPoem.content.split(" ").slice(0, 120).join(" ");
-      // Update the poem preview text
-      document.getElementById('poem-preview').textContent = `${poemPreview}... `;
-    })
-    .catch(err => console.log(err));
+document.getElementById('menuButton').addEventListener('click', openNav);
+document.getElementById('closeButton').addEventListener('click', closeNav);
 
-  fetch('/static/writings/philosophies.json')
-    .then(response => response.json())
-    .then(philosophies => {
-      // Get the first philosophy
-      const firstPhilosophy = philosophies[0];
-      // Extract the first 200 words
-      const philosophyPreview = firstPhilosophy.content.split(" ").slice(0, 120).join(" ");
-      // Update the philosophy preview text
-      document.getElementById('philosophy-preview').textContent = `${philosophyPreview}... `;
-    })
-    .catch(err => console.log(err));
+// Get all the navigation links
+var navLinks = document.querySelectorAll('.overlay a');
 
-   fetch('/static/writings/god.json')
-    .then(response => response.json())
-    .then(gods => {
-      // Get the first god
-      const firstGod = gods[0];
-      // Extract the first 200 words
-      const godPreview = firstGod.content.split(" ").slice(0, 120).join(" ");
-      // Update the god preview text
-      document.getElementById('god-preview').textContent = `${godPreview}... `;
-    })
-    .catch(err => console.log(err));
-});
-
-
-// JavaScript for Random Thought Generator
-function generateThought() {
-    const thoughts = [
-        "Life is about making an impact, not making an income.",
-        "Whatever the mind of man can conceive and believe, it can achieve.",
-        "Strive not to be a success, but rather to be of value.",
-        "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
-    ];
-    const randomIndex = Math.floor(Math.random() * thoughts.length);
-    document.getElementById("thought").innerHTML = thoughts[randomIndex];
+// Add an event listener to each navigation link
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', closeNav);
 }
+
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+  console.log("Navigation opened"); // This will print to the console when the nav is opened
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+  console.log("Navigation closed"); // This will print to the console when the nav is closed
+}
+
+window.addEventListener('scroll', function() {
+  var scrollMessage = document.getElementById('scroll-message');
+  var titleText = document.querySelector('.title-text');
+  var menuButton = document.getElementById('menuButton');
+  var writingsSection = document.getElementById('writings');
+  var codingSection = document.getElementById('coding-projects');
+
+  if (window.scrollY > 0) {
+    scrollMessage.style.opacity = '0';
+    titleText.style.opacity = '1';
+  } else {
+    scrollMessage.style.opacity = '1';
+    titleText.style.opacity = '0';
+  }
+
+  if (window.scrollY >= writingsSection.offsetTop && window.scrollY < codingSection.offsetTop) {
+    menuButton.className = 'color2';
+  } else if (window.scrollY >= codingSection.offsetTop) {
+    menuButton.className = 'color3';
+  } else {
+    menuButton.className = 'color1';
+  }
+});
